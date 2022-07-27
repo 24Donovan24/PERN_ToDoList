01186437f23c7057b84b1c8e69cc9b534ec31d99
Donovan <donovanlee_25@hotmail.com>
2022-07-27 21:44:57 +0800

    Added Navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { toast } from "react-toastify";
+
+const Navbar = ({ setAuth }) => {
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setAuth(false);
+    toast.success("Logged out successfully!");
+  };
+
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-secondary rounded" style={{fontSize: "1.2em"}}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/" style={{ color: "white", fontSize: "1.5em"}}>
+          Todo List
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                style={{ color: "white" }}
+                aria-current="page"
+                href="/"
+              >
+                Settings
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                style={{ color: "white" }}
+                aria-current="page"
+                onClick={(e) => logout(e)}
+                href="/"
+              >
+                Logout
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
