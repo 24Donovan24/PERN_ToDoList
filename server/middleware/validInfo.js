cadb13744bd25bd2f821cca69adbed119d7b9fa1
Donovan <donovanlee_25@hotmail.com>
2022-08-01 17:09:08 +0800

    Added another case to handle the /changeprofile route

--- server/middleware/validInfo.js
+++ server/middleware/validInfo.js
@@ -1,12 +1,12 @@
 module.exports = function(req, res, next) {
     const { email, name, password } = req.body;
-  
+    const { newEmail, newName, newPassword } = req.body;  
     function validEmail(userEmail) {
       return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
     }
   
     if (req.path === "/register") {
-      console.log(!email.length);
+      // console.log(!email.length);
       if (![email, name, password].every(Boolean)) {
         return res.status(401).json("Missing Credentials");
       } else if (!validEmail(email)) {
@@ -17,6 +17,12 @@ module.exports = function(req, res, next) {
         return res.status(401).json("Missing Credentials");
       } else if (!validEmail(email)) {
         return res.status(401).json("Invalid Email");
+      }
+    } else if (req.path === "/changeprofile") {
+      if (![newEmail, newName, newPassword].every(Boolean)) {
+        return res.status(400).json("Missing credentials!");
+      } else if (!validEmail(newEmail)) {
+        return res.status(400).json("Invalid Email");
       }
     }
   
